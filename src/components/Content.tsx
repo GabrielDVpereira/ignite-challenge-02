5ae7d630b1ba2096791fa19b64fcc19d113f46ae
Gabriel Davi <gdavi.gd@gmail.com>
2021-03-16 00:45:06 -0300

    resolve challenge

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,47 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface Genre {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface Movie {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  selectedGenre: Genre;
+  movies: Movie[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
